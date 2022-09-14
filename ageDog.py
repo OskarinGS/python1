725a36c9e17dba3b6f46b0bf2fef6683d9e35b0c
guillen <oskar_gusan@hotmail.com>
2022-09-14 10:59:37 -0500

    Actuzalizacion 3

---
+++ ageDog.py
@@ -0,0 +1,8 @@
+edad = int(input("Ingresa tu edad: "))
+last_year = edad - 2
+first_year = 2 * 10.5
+last_year = last_year * 4
+edadDog = last_year + first_year
+
+
+print (f"Tienes {edad} años que equivalen a {edadDog} años perrunos") 
--- dollar.py
+++ dollar.py
@@ -1,4 +1,4 @@
 dolar = input("Ingresa la cantidad de dolares: ")
-cambio = 20.06
+cambio = 22.23
 pesos  = int(dolar) * cambio
 print (f"Los {dolar} dolares son: {pesos} pesos")
