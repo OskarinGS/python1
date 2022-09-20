d56df96b38b41774f3f7c3d3f8f6b946df0b107c
guillen <oskar_gusan@hotmail.com>
2022-09-20 09:26:17 -0500

    Cuarto commit

---
+++ condicionales.py
@@ -0,0 +1,12 @@
+# Condicionales if elif else 
+# Nos permite evaluar expresiones booleanas que de cumplirse realizan
+# Una accion y en caso de que no entonces realizan otra 
+
+# Evaluar si una persona es mayor de edad
+
+age = int(input('Ingresa tu edad: '))
+
+if age >= 18:
+    print("Eres mayor de edad¡")
+else:
+    print("Eres menos de edad¡")    
