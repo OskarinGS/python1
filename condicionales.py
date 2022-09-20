4d5c0aba828d2a536945a723c9dfe3e9e2381d1e
guillen <oskar_gusan@hotmail.com>
2022-09-20 09:57:31 -0500

    Quinto commit

--- condicionales.py
+++ condicionales.py
@@ -3,10 +3,30 @@
 # Una accion y en caso de que no entonces realizan otra 
 
 # Evaluar si una persona es mayor de edad
+# Nos diga es niño, joven, adulto, bebe, muy mayor
 
+genre = input("Ingresa tu género (H/M): ")
 age = int(input('Ingresa tu edad: '))
 
-if age >= 18:
-    print("Eres mayor de edad¡")
+mujer = ""
+
+if genre == "M":
+    mujer = "a"
+
+if age < 2:
+    print(f"Eres un{mujer} bebé")
+elif age < 12:
+    print(f"Eres un{mujer} niño")    
+elif age < 18:
+    print(f"Eres un{mujer} joven") 
+elif age < 50:
+    print(f"Eres un{mujer} adulto")         
 else:
-    print("Eres menos de edad¡")    
+    print(f"Eres muy mayor")    
+
+#if genre == "H":
+#    print("Eres un Hombre")
+# elif genre == "M":
+#    print("Eres una mujer") 
+# else:
+#    print("Eres un extraterreste")   
